5dad6d493967de87ca888c0c032c78988824819e
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 15:11:03 -0300

    Create JWTUtil

---
+++ ms-auth-services/src/main/java/com/santander/msauthservices/util/JWTUtil.java
@@ -0,0 +1,77 @@
+package com.santander.msauthservices.util;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+@AllArgsConstructor
+public class JWTUtil {
+
+    private final Environment environment;
+
+    public String extractUsername(String token) {
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public Date extractExpiration(String token) {
+        return extractClaim(token, Claims::getExpiration);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        return claimsResolver.apply(extractAllClaims(token));
+    }
+
+    private Claims extractAllClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(getKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    public String generateToken(String username) {
+        Map<String, Object> claims = new HashMap<>();
+        return createToken(claims, username);
+    }
+
+
+    private String createToken(Map<String, Object> claims, String subject) {
+
+        return Jwts.builder()
+                .claims(claims)
+                .subject(subject)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 10))
+                .signWith(getKey())
+                .compact();
+    }
+
+    public Boolean validateToken(String token, UserDetails userDetails) {
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isTokenExpired(token));
+    }
+
+    private SecretKey getKey() {
+        String SECRET_KEY = "spring.security.oauth2.resourceserver.jwt.public-key-location";
+        return Keys.hmacShaKeyFor(
+                environment.getRequiredProperty(SECRET_KEY)
+                        .getBytes(StandardCharsets.UTF_8)
+        );
+    }
+}
