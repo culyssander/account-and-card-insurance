f179dfb150d34777eb4a0448b426bca0393ae613
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 11:08:50 -0300

    Create ms Auth Services

---
+++ ms-auth-services/src/main/java/com/santander/msauthservices/MsAuthServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.msauthservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAuthServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAuthServicesApplication.class, args);
+	}
+
+}
---
+++ ms-auth-services/src/test/java/com/santander/msauthservices/MsAuthServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.msauthservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
