4762f014ecbfa42e82cd3c82126ea1b0c43721bd
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:48:37 -0300

    Add policy project

---
+++ ms-policy-services/src/main/java/com/santander/mspolicyservices/MsPolicyServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.mspolicyservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPolicyServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsPolicyServicesApplication.class, args);
+	}
+
+}
---
+++ ms-policy-services/src/test/java/com/santander/mspolicyservices/MsPolicyServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.mspolicyservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPolicyServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
