08dc2436e24314fd2098fe922c1890d946fed900
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:49:38 -0300

    Add claim questionnaire project

---
+++ ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/MsClaimsQuestionnaireServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.msclaimsquestionnaireservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClaimsQuestionnaireServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClaimsQuestionnaireServicesApplication.class, args);
+	}
+
+}
