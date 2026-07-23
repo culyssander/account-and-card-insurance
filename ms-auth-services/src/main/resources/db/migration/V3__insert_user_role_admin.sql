697688ac2d5615fb18ec66b5d9e6833bdd6dda6b
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 15:06:21 -0300

    insert new user

---
+++ ms-auth-services/src/main/resources/db/migration/V3__insert_user_role_admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO public.user_
+(active, id, insured_id, created_at, email, "name", "password", "role")
+VALUES(true, 1, NULL, '2026-07-23 14:11:16.428', 'quitumbaferreira@test.com', 'Quitumba', '$2a$10$NDl52uHKH8k/GqVwx8TIHO0Us41MBXPNGU00v00Y2AeunpzB7Tame', 'ADMIN');
