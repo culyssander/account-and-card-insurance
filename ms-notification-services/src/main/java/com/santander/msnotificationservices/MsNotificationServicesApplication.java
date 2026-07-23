1f23b4a5010c4203a6d42cd7497b97a89e7835f2
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:50:27 -0300

    Add notification project

---
+++ ms-notification-services/src/main/java/com/santander/msnotificationservices/MsNotificationServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.msnotificationservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotificationServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsNotificationServicesApplication.class, args);
+	}
+
+}
---
+++ ms-notification-services/src/test/java/com/santander/msnotificationservices/MsNotificationServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.msnotificationservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNotificationServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
