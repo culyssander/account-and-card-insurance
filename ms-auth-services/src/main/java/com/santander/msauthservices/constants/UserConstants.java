429dd490ec958c8eb8d62fad53f245b544fc0d13
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:47:45 -0300

    add log information

--- ms-auth-services/src/main/java/com/santander/msauthservices/constants/UserConstants.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/constants/UserConstants.java
@@ -7,6 +7,7 @@ public final class UserConstants {
     public final static String USER_ALREADY_EXISTS = "user.already_exists";
     public final static String USER_NOT_FOUND = "user.not_found";
     public final static String USER_LOGIN_INVALID = "user.login_invalid";
+    public final static String USER_LOGIN_VALID = "user.login_valid";
     public final static String ARGUMENT_INVALID = "argument.invalid";
     public final static String INSURED_INVALID_PHONE_NUMBER = "user.invalid_phone_number";
 }
