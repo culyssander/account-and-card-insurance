40d8d5a9186cae27d400bd8da656d12b379a1d6e
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:49:07 -0300

    Add claim project

---
+++ ms-claims-services/src/main/java/com/santander/msclaimsservices/MsClaimsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.msclaimsservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsClaimsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsClaimsServicesApplication.class, args);
+	}
+
+}
---
+++ ms-claims-services/src/test/java/com/santander/msclaimsservices/MsClaimsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.msclaimsservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClaimsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
