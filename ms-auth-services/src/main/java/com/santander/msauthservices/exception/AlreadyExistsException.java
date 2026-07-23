efb105e033ea947e332b75df368938192e3417c1
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 15:03:57 -0300

    Create entityException

---
+++ ms-auth-services/src/main/java/com/santander/msauthservices/exception/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.santander.msauthservices.exception;
+
+public class AlreadyExistsException extends RuntimeException {
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
