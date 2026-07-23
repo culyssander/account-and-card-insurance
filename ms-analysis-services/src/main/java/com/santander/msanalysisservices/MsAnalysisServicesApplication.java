c6952e9419dca54fc7379c0c62b0edfe091663b9
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 16:49:57 -0300

    Add claim analysis project

---
+++ ms-analysis-services/src/main/java/com/santander/msanalysisservices/MsAnalysisServicesApplication.java
@@ -0,0 +1,13 @@
+package com.santander.msanalysisservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAnalysisServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsAnalysisServicesApplication.class, args);
+	}
+
+}
---
+++ ms-analysis-services/src/test/java/com/santander/msanalysisservices/MsAnalysisServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.santander.msanalysisservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAnalysisServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
