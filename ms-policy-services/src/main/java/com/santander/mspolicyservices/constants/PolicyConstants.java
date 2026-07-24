61e4d311e387dec70b64b72157f0730ed928a96e
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 21:54:53 -0300

    Create policyConstants

---
+++ ms-policy-services/src/main/java/com/santander/mspolicyservices/constants/PolicyConstants.java
@@ -0,0 +1,10 @@
+package com.santander.mspolicyservices.constants;
+
+public final class PolicyConstants {
+
+    private PolicyConstants () {}
+
+    public static final String POLICY_NOT_FOUND = "policy.not_found";
+    public static final String POLICY_ACCESS_DENIED = "policy.access_denied";
+
+}
