1cd191289f1238d1778be199775670c738e36982
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-23 23:25:42 -0300

    Add @EnableDiscoveryClient in start classes

--- ms-auth-services/src/main/java/com/santander/msauthservices/MsAuthServicesApplication.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/MsAuthServicesApplication.java
@@ -2,7 +2,9 @@ package com.santander.msauthservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MsAuthServicesApplication {
 
