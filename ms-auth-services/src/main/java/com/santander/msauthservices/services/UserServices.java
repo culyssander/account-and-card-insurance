ea5054e7ee9bf29bb16540343fb7570edf8971f7
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-25 21:40:42 -0300

    add new method to return user logged

--- ms-auth-services/src/main/java/com/santander/msauthservices/services/UserServices.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/services/UserServices.java
@@ -4,11 +4,14 @@ import com.santander.msauthservices.constants.UserConstants;
 import com.santander.msauthservices.dto.UserRequestDto;
 import com.santander.msauthservices.dto.UserResponseDto;
 import com.santander.msauthservices.exception.AlreadyExistsException;
+import com.santander.msauthservices.exception.BusinessException;
 import com.santander.msauthservices.exception.NotFoundException;
 import com.santander.msauthservices.model.Insured;
 import com.santander.msauthservices.model.Role;
 import com.santander.msauthservices.model.User;
 import com.santander.msauthservices.repository.UserRepository;
+import com.santander.msauthservices.util.JWTUtil;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.AllArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.MessageSource;
@@ -17,10 +20,13 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 import java.util.Locale;
+import java.util.Objects;
 
 @Service
 @AllArgsConstructor
@@ -30,6 +36,7 @@ public class UserServices implements UserDetailsService {
     private MessageSource messageSource;
     private InsuredServices insuredServices;
     private ModelMapper modelMapper;
+    private JWTUtil jwtUtil;
 
     public UserResponseDto newUser(UserRequestDto request, Locale locale) {
         validateUserByEmail(request.getEmail(), locale);
@@ -61,7 +68,7 @@ public class UserServices implements UserDetailsService {
 
     public User findByEmail(String email, Locale locale) {
         return userRepository.findByEmail(email)
-                .orElseThrow(() -> new NotFoundException(messageSource.getMessage(UserConstants.USER_NOT_FOUND, new Object[] {}, locale)));
+                .orElseThrow(() -> new NotFoundException(messageSource.getMessage(UserConstants.USER_NOT_FOUND, new Object[] {email}, locale)));
     }
 
     private void validateUserByEmail(String email, Locale locale) {
@@ -91,5 +98,31 @@ public class UserServices implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return findByEmail(username, Locale.ENGLISH);
+    }
+
+    private String getLoggedUser() {
+        HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.currentRequestAttributes()).getRequest();
+        String token = request.getHeader("Authorization").split(" ")[1];
+        return jwtUtil.extractUsername(token);
+    }
+
+    private User logged(Locale locale) throws BusinessException {
+        String email = getLoggedUser();
+
+        User user = findByEmail(email, locale);
+
+        if (Objects.nonNull(user))
+            return user;
+
+        throw new BusinessException("Error with logged-in user");
+    }
+
+    public String userLogged(Locale locale) {
+        User user = logged(locale);
+
+        if (!user.getRole().equals(Role.INSURED))
+            throw new BusinessException("Not permission");
+
+        return user.getEmail();
     }
 }
