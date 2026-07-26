e27342d46e8ad902f949945c1e8b6bd706a3bc9b
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-25 21:38:19 -0300

    Add @RequestParam in ProtocolController.java

--- ms-protocol-services/src/main/java/com/santander/protocoloservice/controller/ProtocolController.java
+++ ms-protocol-services/src/main/java/com/santander/protocoloservice/controller/ProtocolController.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,7 @@ public class ProtocolController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
-    public String createProtocol(String code) {
+    public String createProtocol(@RequestParam String code) {
         return protocolSequencialServicos.generateProtocol(code);
     }
 }
