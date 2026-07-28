506bd879bf818b959bfd0c7bf0960ad3375322ac
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:52:14 -0300

    Add claimResponse in questionnaire

---
+++ ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/dto/ClaimResponseDto.java
@@ -0,0 +1,29 @@
+package com.santander.msclaimsquestionnaireservices.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDateTime;
+
+
+@Data
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+@AllArgsConstructor
+@NoArgsConstructor
+public class ClaimResponseDto {
+    private BigInteger id;
+    private String claimNumber;
+    private String status;
+    private String description;
+    private LocalDateTime eventDate;
+    private LocalDateTime openingDate;
+    private BigDecimal claimedAmount;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+}
