99f33cbd72f9ca7ca3f6bd9078a50c7d7cbc9ea8
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:45:09 -0300

    Add feign dependency in questionnaire project

--- ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/MsClaimsQuestionnaireServicesApplication.java
+++ ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/MsClaimsQuestionnaireServicesApplication.java
@@ -2,7 +2,9 @@ package com.santander.msclaimsquestionnaireservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MsClaimsQuestionnaireServicesApplication {
 
