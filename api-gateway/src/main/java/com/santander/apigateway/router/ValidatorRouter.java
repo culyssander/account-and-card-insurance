bbd112d824cfa75a8b7f22bb6e081a005fd3dade
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:52:58 -0300

    Create ValidatorRouter in gateway

---
+++ api-gateway/src/main/java/com/santander/apigateway/router/ValidatorRouter.java
@@ -0,0 +1,19 @@
+package com.santander.apigateway.router;
+
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+@Component
+public class ValidatorRouter {
+
+    private static  final List<String> OPEN_API_ENDPOINTS = List.of(
+            "/swagger-ui.html",
+            "/api/v1/login"
+    );
+
+    public Predicate<ServerHttpRequest> isSecure = request -> OPEN_API_ENDPOINTS.stream()
+            .noneMatch(uri -> request.getURI().getPath().contains(uri));
+}
