3b83a2e1b13a99a56a562982e7a763c54a01b60a
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:46:00 -0300

    Update endpoint in PolicyController

--- ms-protocol-services/src/main/java/com/santander/protocoloservice/controller/ProtocolController.java
+++ ms-protocol-services/src/main/java/com/santander/protocoloservice/controller/ProtocolController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/protocols")
+@RequestMapping("/")
 @AllArgsConstructor
 public class ProtocolController {
 
