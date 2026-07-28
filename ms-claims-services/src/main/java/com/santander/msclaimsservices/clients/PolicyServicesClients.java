929a5dab92cd0b8c5a53a1fc3f1510a573b960cf
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:45:33 -0300

    Config clients in PolicyServicesClients

--- ms-claims-services/src/main/java/com/santander/msclaimsservices/clients/PolicyServicesClients.java
+++ ms-claims-services/src/main/java/com/santander/msclaimsservices/clients/PolicyServicesClients.java
@@ -1,14 +1,15 @@
 package com.santander.msclaimsservices.clients;
 
+import com.santander.msclaimsservices.config.FeignConfig;
 import com.santander.msclaimsservices.dto.PolicyResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "policyServicesClients", url = "${policy.services.url}")
+@FeignClient(name = "MS-POLICY-SERVICES", configuration = FeignConfig.class)
 public interface PolicyServicesClients {
 
-    @GetMapping("/policy-number/{policyNumber}")
+    @GetMapping("/v1/policies/insured/policy-number/{policyNumber}")
     PolicyResponseDto findByPolicyNumber(@PathVariable String policyNumber);
 
 }
