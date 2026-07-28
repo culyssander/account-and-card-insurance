c1971ec67b5d28e1ebaa45a335283013f9157df8
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:00:02 -0300

    Add name of services to eureka

--- ms-claims-services/src/main/java/com/santander/msclaimsservices/clients/ProtocolSequencialClients.java
+++ ms-claims-services/src/main/java/com/santander/msclaimsservices/clients/ProtocolSequencialClients.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "protocolSequencialClients", url = "${protocol.services.url}")
+@FeignClient(name = "MS-PROTOCOL-SERVICES")
 public interface ProtocolSequencialClients {
 
     @PostMapping
