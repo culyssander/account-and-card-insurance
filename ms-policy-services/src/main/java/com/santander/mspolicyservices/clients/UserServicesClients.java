2debeeca1f43b0fb54ef9b54e46938dec741dea4
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-28 00:47:33 -0300

    Update UserServicesClients

--- ms-policy-services/src/main/java/com/santander/mspolicyservices/clients/UserServicesClients.java
+++ ms-policy-services/src/main/java/com/santander/mspolicyservices/clients/UserServicesClients.java
@@ -1,18 +1,20 @@
 package com.santander.mspolicyservices.clients;
 
+import com.santander.mspolicyservices.config.FeignConfig;
 import com.santander.mspolicyservices.dto.UserResponseDto;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "userServicesClients", url = "${auth.services.url}")
+//@FeignClient(name = "userServicesClients", url = "${auth.services.url}")
+@FeignClient(name = "MS-AUTH-SERVICES", configuration = FeignConfig.class)
 public interface UserServicesClients {
 
     @GetMapping(path = "/users/email/{email}")
     UserResponseDto findByEmail(@PathVariable String email);
 
-    @GetMapping(path = "/insureds/cpf/{cpf}")
-    UserResponseDto findByCpf(@PathVariable String cpf);
+    @GetMapping(path = "/v1/insureds/logged")
+    UserResponseDto findByInsuredLogged();
 
     @GetMapping(path = "/users/user-logged")
     UserResponseDto findByUserLogged();
