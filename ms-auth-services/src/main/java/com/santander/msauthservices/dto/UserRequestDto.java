80327c1e48eaf3fe073902f647483ad76eb4b139
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-07-29 23:28:13 -0300

    Add @builder and anotations for constructor

--- ms-auth-services/src/main/java/com/santander/msauthservices/dto/UserRequestDto.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/dto/UserRequestDto.java
@@ -4,8 +4,14 @@ import com.santander.msauthservices.model.Role;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class UserRequestDto {
     @NotBlank
--- ms-auth-services/src/main/java/com/santander/msauthservices/dto/UserResponseDto.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/dto/UserResponseDto.java
@@ -1,11 +1,17 @@
 package com.santander.msauthservices.dto;
 
 import com.santander.msauthservices.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigInteger;
 import java.time.LocalDateTime;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Data
 public class UserResponseDto {
     private BigInteger id;
