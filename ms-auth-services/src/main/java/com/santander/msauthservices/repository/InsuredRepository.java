a3123b0f3290c1f79225372f8d82728d239b9bbb
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:59:23 -0300

    Update InsureRepository

--- ms-auth-services/src/main/java/com/santander/msauthservices/repository/InsuredRepository.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/repository/InsuredRepository.java
@@ -3,10 +3,9 @@ package com.santander.msauthservices.repository;
 import com.santander.msauthservices.model.Insured;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.math.BigInteger;
 import java.util.Optional;
 
-public interface InsuredRepository extends JpaRepository<Insured, BigInteger> {
+public interface InsuredRepository extends JpaRepository<Insured, Long> {
 
     boolean existsByCpf(String cpf);
 
