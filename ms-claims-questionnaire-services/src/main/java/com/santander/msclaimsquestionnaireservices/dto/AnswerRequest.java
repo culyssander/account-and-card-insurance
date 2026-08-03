e6c8a3b9b1f8bcfe4ff9c0aa5159b65aa84a5900
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:52:45 -0300

    remove claimId and and add PathVariabel

--- ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/dto/AnswerRequest.java
+++ ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/dto/AnswerRequest.java
@@ -3,7 +3,6 @@ package com.santander.msclaimsquestionnaireservices.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record AnswerRequest(
-        @NotBlank String claimId,
         @NotBlank String questionId,
         @NotBlank String selectedOption
 ) {
