b512b38b2e990337aaeb1eed75e964c8d3bce42d
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 02:01:17 -0300

    Create QuestionnaireConstants

---
+++ ms-claims-questionnaire-services/src/main/java/com/santander/msclaimsquestionnaireservices/constants/QuestionnaireConstants.java
@@ -0,0 +1,10 @@
+package com.santander.msclaimsquestionnaireservices.constants;
+
+public class QuestionnaireConstants {
+
+    private QuestionnaireConstants() {}
+
+    public static final String RABBIT_QUESTIONNAIRE_COMPLETE_EXCHANGE        = "QUESTIONNAIRE.COMPLETE.EXCHANGE";
+    public static final String RABBIT_QUEUE_QUESTIONNAIRE_COMPLETE_ROUTER    = "QUESTIONNAIRE.COMPLETE.KEY";
+    public static final String RABBIT_QUEUE_QUESTIONNAIRE_COMPLETE           = "QUESTIONNAIRE.COMPLETE";
+}
