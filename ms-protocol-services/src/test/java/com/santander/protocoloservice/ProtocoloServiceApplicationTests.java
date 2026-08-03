3a8cfafdc86342ca73e1947e2d57bf84e59b6681
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:29:33 -0300

    Add @ActiveProfiles("test")

--- ms-protocol-services/src/test/java/com/santander/protocoloservice/ProtocoloServiceApplicationTests.java
+++ ms-protocol-services/src/test/java/com/santander/protocoloservice/ProtocoloServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.santander.protocoloservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ProtocoloServiceApplicationTests {
 
 	@Test
