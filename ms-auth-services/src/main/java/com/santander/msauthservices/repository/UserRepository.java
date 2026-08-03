1e0ce79bfdb0ea4c657b75b40775664c765cb114
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 02:04:52 -0300

    Update UserRepository

--- ms-auth-services/src/main/java/com/santander/msauthservices/repository/UserRepository.java
+++ ms-auth-services/src/main/java/com/santander/msauthservices/repository/UserRepository.java
@@ -2,11 +2,12 @@ package com.santander.msauthservices.repository;
 
 import com.santander.msauthservices.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-import java.math.BigInteger;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, BigInteger> {
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
