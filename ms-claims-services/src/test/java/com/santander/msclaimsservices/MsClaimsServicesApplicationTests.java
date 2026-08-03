3364e94ff172cc6a198b10f867d9acd19c43c408
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:28:40 -0300

    add @ActiveProfiles("test")

--- ms-claims-services/src/test/java/com/santander/msclaimsservices/MsClaimsServicesApplicationTests.java
+++ ms-claims-services/src/test/java/com/santander/msclaimsservices/MsClaimsServicesApplicationTests.java
@@ -2,8 +2,14 @@ package com.santander.msclaimsservices;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import software.amazon.awssdk.services.s3.S3Client;
 
 @SpringBootTest
+@ActiveProfiles("test")
+@Import(TestS3Config.class)
 class MsClaimsServicesApplicationTests {
 
 	@Test
