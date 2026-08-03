093f9ef7b0751aa23549113dc1e7a07a203de04d
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:48:06 -0300

    seed insured

---
+++ ms-auth-services/src/main/resources/db/migration/V3__insert_insured.sql
@@ -0,0 +1,3 @@
+INSERT INTO insured (id, name, cpf, email, phone) VALUES
+                                                  (1, 'João da Silva',      '12345678901', 'joao.silva@email.com',      '11987654321'),
+                                                  (2, 'Maria Oliveira',     '12345678902', 'maria.oliveira@email.com',  '11987654322');
