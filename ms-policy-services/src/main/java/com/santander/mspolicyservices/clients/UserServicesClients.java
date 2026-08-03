62532cac079ba0dae8d6dee6eaa80d1bdb99668c
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:54:17 -0300

    update path in User Clients

--- ms-policy-services/src/main/java/com/santander/mspolicyservices/clients/UserServicesClients.java
+++ ms-policy-services/src/main/java/com/santander/mspolicyservices/clients/UserServicesClients.java
@@ -6,16 +6,15 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-//@FeignClient(name = "userServicesClients", url = "${auth.services.url}")
 @FeignClient(name = "MS-AUTH-SERVICES", configuration = FeignConfig.class)
 public interface UserServicesClients {
 
-    @GetMapping(path = "/users/email/{email}")
+    @GetMapping(path = "/v1/users/email/{email}")
     UserResponseDto findByEmail(@PathVariable String email);
 
     @GetMapping(path = "/v1/insureds/logged")
     UserResponseDto findByInsuredLogged();
 
-    @GetMapping(path = "/users/user-logged")
+    @GetMapping(path = "/v1/users/user-logged")
     UserResponseDto findByUserLogged();
 }
