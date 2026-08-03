f2cdc342ee36f631588bd1cd489e96291e236672
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:55:44 -0300

    create table im protocolo test

---
+++ ms-protocol-services/src/test/resources/schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE protocol_sequencial (
+                                     id BIGINT PRIMARY KEY,
+                                     product_code VARCHAR(255),
+                                     protocol_year INTEGER,
+                                     last_number INTEGER
+);
