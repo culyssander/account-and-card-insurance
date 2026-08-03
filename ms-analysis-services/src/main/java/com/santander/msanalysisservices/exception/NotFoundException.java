41ac12e79de7d0842d96420caa52e78f0e79ec8e
Quitumba Ferreira <culyssandercentro@gmail.com>
2026-08-03 01:34:44 -0300

    Exception classes

---
+++ ms-analysis-services/src/main/java/com/santander/msanalysisservices/exception/NotFoundException.java
@@ -0,0 +1,7 @@
+package com.santander.msanalysisservices.exception;
+
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String message) {
+        super(message);
+    }
+}
